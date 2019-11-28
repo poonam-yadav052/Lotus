6b88b2739e34c3c7c29af75aa03f35c35f9c70b0
Poonam Yadav <poonamyadav@Poonams-MacBook-Air.local>
2019-11-28 21:05:26 +0530

    pull latest6

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder,FormGroup,FormsModule,ReactiveFormsModule,FormControl,Validators,Validator } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, FormControl, Validators, Validator } from '@angular/forms';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BsModalRef, BsModalService, CarouselConfig } from 'ngx-bootstrap';
 import { UploadFileService } from 'src/app/upload-file.service';
@@ -11,56 +11,69 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  loginForm:FormGroup;
+  loginForm: FormGroup;
   submitted = false;
-  result:any;
+  result: any;
+  userdata: any;
+  userrole: any;
   constructor(private formBuilder: FormBuilder,
-    private service:UploadFileService,
-    private spinner:NgxSpinnerService,
-    private modalservice: BsModalService,private router:Router) { 
+    private service: UploadFileService,
+    private spinner: NgxSpinnerService,
+    private modalservice: BsModalService, private router: Router) {
     this.loginForm = this.formBuilder.group({
-      username: ['Test123',[Validators.required]],
-      password: ['Test123',[Validators.required]]
+      username: ['Test123', [Validators.required]],
+      password: ['Test123', [Validators.required]]
     });
   }
 
   ngOnInit() {
   }
-  get f() {return this.loginForm.controls}
+  get f() { return this.loginForm.controls }
 
   onSubmit() {
+
     console.log("in submit");
     this.submitted = true;
     // stop here if form is invalid
     if (this.loginForm.invalid) {
-        return;
+      return;
     }
     this.spinner.show();
     // Process checkout data here
-    
-    this.service.Login(this.f.username.value,this.f.password.value).subscribe(
-      data=>{
+
+    this.service.Login(this.f.username.value, this.f.password.value).subscribe(
+      data => {
         console.log("Success")
         console.log(data)
-        this.spinner.hide(); 
-          if(data.result == "Invalid login details!")     
-          {
+        this.spinner.hide();
+        if (data.result == "Invalid login details!") {
 
-          }
-          else{
-          this.result=data;           
+        }
+        else {
+          this.result = data;
           localStorage.clear();
-          localStorage.setItem('userdetails',JSON.stringify(data.result[0]))
-          this.router.navigate(['../home/']);     
-          } 
+          localStorage.setItem('userdetails', JSON.stringify(data.result[0]))
+
+          this.userdata = JSON.parse(localStorage.getItem('userdetails'));
+
+          this.userrole = this.userdata["userRole"];
+          console.log("this.userrole", this.userdata)
+          if (this.userrole == 5) {
+            alert("User can not login")
+          } else {
+            this.router.navigate(['../home/']);
+           }
+         
+        }
       },
-      error=>{
+      error => {
         this.spinner.hide();
         console.log("Error")
         console.log(error)
-        this.result=error.error.Error;
+        this.result = error.error.Error;
       })
-    
+
+
   }
 
 }
--- src/app/pages/adduser/adduser.component.ts
+++ src/app/pages/adduser/adduser.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
-import {MatDialog} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { UploadFileService } from 'src/app/upload-file.service';
 import { NgxSpinnerService } from 'ngx-spinner';
@@ -14,61 +14,60 @@ export class AdduserComponent implements OnInit {
   submitted = false;
   userprefix;
   userForm: FormGroup;
-  userdata:any;
-  creditlimit:number
-  userrole:number;
+  userdata: any;
+  creditlimit: number
+  userrole: number;
   constructor(private formBuilder: FormBuilder,
     public router: Router,
-    private service:UploadFileService,private spinner:NgxSpinnerService) { }
+    private service: UploadFileService, private spinner: NgxSpinnerService) { }
 
   ngOnInit() {
 
-    this.userdata=JSON.parse(localStorage.getItem('userdetails'));
-    this.creditlimit=this.userdata["creditLimit"]
-    this.userrole=this.userdata["userRole"]
+    this.userdata = JSON.parse(localStorage.getItem('userdetails'));
+    this.creditlimit = this.userdata["creditLimit"]
+    this.userrole = this.userdata["userRole"]
     this.userForm = this.formBuilder.group({
-  		loginName: ['', Validators.required],
+      loginName: ['', Validators.required],
       status: ['', [Validators.required]],
       password: ['', [Validators.required]],
-      resetpassword:['', [Validators.required]],
+      resetpassword: ['', [Validators.required]],
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
 
-      
+
     });
   }
 
-  get f() {return this.userForm.controls}
+  get f() { return this.userForm.controls }
 
-  onSubmit()
-  {
+  onSubmit() {
     console.log("submit")
     this.submitted = true;
     // stop here if form is invalid
     console.log(this.userForm.invalid)
     if (this.userForm.invalid) {
-        return;
+      return;
     }
     let data: any = Object.assign(this.userForm.value);
     console.log(data)
     this.spinner.show();
-    data.createdBy=this.userrole;
-    data.userRole=5;
+    data.createdBy = this.userrole;
+    data.userRole = 5;
     this.service.adduser(data).subscribe(
-    data=>{
-    this.spinner.hide();
-    console.log(data);
-    let path = '/userlist';
-    this.router.navigate([path]);
-    this.registered = true;
-    },
-    error=>{
-    console.log(error)
-    this.spinner.hide();
-    }
+      data => {
+        this.spinner.hide();
+        console.log(data);
+        let path = '/userlist';
+        this.router.navigate([path]);
+        this.registered = true;
+      },
+      error => {
+        console.log(error)
+        this.spinner.hide();
+      }
 
     )
-    this.registered = true;   
+    this.registered = true;
   }
 
 }
